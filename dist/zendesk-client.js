0e6d78677ff009448e7e29a6c1b6730098d62818
Hideyuki Utsunomiya <hidechae@gmail.com>
2025-05-14 12:19:58 +0900

    update dist

--- dist/zendesk-client.js
+++ dist/zendesk-client.js
@@ -46,20 +46,20 @@ export class ZendeskClient {
         // 連続した空白行を削除
         const multipleLineBreaksPattern = /\n\s*\n/g;
         // 空のタグを削除
-        let cleanedHtml = html.replace(emptyTagPattern, '');
+        let cleanedHtml = html.replace(emptyTagPattern, "");
         // 不要な属性を削除
-        cleanedHtml = cleanedHtml.replace(styleAttributePattern, '');
-        cleanedHtml = cleanedHtml.replace(idAttributePattern, '');
-        cleanedHtml = cleanedHtml.replace(classAttributePattern, '');
-        cleanedHtml = cleanedHtml.replace(targetAttributePattern, '');
-        cleanedHtml = cleanedHtml.replace(borderAttributePattern, '');
+        cleanedHtml = cleanedHtml.replace(styleAttributePattern, "");
+        cleanedHtml = cleanedHtml.replace(idAttributePattern, "");
+        cleanedHtml = cleanedHtml.replace(classAttributePattern, "");
+        cleanedHtml = cleanedHtml.replace(targetAttributePattern, "");
+        cleanedHtml = cleanedHtml.replace(borderAttributePattern, "");
         // 不要なタグを削除してテキストのみを残す
-        cleanedHtml = cleanedHtml.replace(spanTagPattern, '$1');
-        cleanedHtml = cleanedHtml.replace(fontTagPattern, '$1');
+        cleanedHtml = cleanedHtml.replace(spanTagPattern, "$1");
+        cleanedHtml = cleanedHtml.replace(fontTagPattern, "$1");
         // 連続した空白行を1つの改行に置換
-        cleanedHtml = cleanedHtml.replace(multipleLineBreaksPattern, '\n');
+        cleanedHtml = cleanedHtml.replace(multipleLineBreaksPattern, "\n");
         // テーブルのセル内の余分な改行を削除
-        cleanedHtml = cleanedHtml.replace(/<td>\s*<p>(.*?)<\/p>\s*<\/td>/gi, '<td>$1</td>');
+        cleanedHtml = cleanedHtml.replace(/<td>\s*<p>(.*?)<\/p>\s*<\/td>/gi, "<td>$1</td>");
         return cleanedHtml;
     }
     // Search for articles
@@ -107,7 +107,7 @@ export class ZendeskClient {
                 updated_at: data.article.updated_at,
                 title: data.article.title,
                 label_names: data.article.label_names,
-                body: this.cleanHtmlContent(data.article.body)
+                body: this.cleanHtmlContent(data.article.body),
             };
             data.article = filteredArticle;
         }
